929a1561d0ee243c626402cf81b13411cabacd32
Adeyemi Arimoro <arimoro71@gmail.com>
2023-03-21 15:24:08 -0500

    Adds solution to index.js

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers (drivers) {
+    return drivers.slice(0, 2)
+}
+
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2)
+}
+    
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+function createFareMultiplier(num){
+    return function (fairMultiplier){ return num * fairMultiplier; };
+
+}
+
+
+const fareDoubler = function (fairMultiplier){return 2 * fairMultiplier;};
+
+const fareTripler = function (fairMultiplier){return 3 * fairMultiplier;};
+
+function selectDifferentDrivers(arrayOfDrivers, myFunction){
+    return myFunction(arrayOfDrivers);
+}
